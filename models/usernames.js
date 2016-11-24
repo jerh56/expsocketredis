f5ae3e044a418ecb0a5d3b08caf25e0f6ff192cb
Johnny <jerh56@gmail.com>
2016-11-23 22:50:46 -0700

    Se agregó un atributo más a la estructura del collection usernames y también se modificó socketio.js para almacenar el idroom del usuario que se obtiene por medio de UUID.

--- models/usernames.js
+++ models/usernames.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 var userSchema = mongoose.Schema({
     nombre: String,
-    socketid: String
+    socketid: String,
+    idroom: String
 });
 
 module.exports = mongoose.model('usernames', userSchema);
